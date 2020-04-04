259819140d3e14864c84462df64c2f23786f4f12
Ket93 <kevincui113@gmail.com>
2020-04-04 17:09:07 -0400

    Added map class

---
+++ src/Pokemon.java
@@ -0,0 +1,21 @@
+public class Pokemon {
+    private String name,type,strong,weak;
+    private int hp,attack,defense,speed,level;
+
+    public Pokemon (String line){
+        String [] stats = line.split(" ");
+        name = stats[1];
+        type = stats [2];
+        hp = Integer.parseInt(stats[3]);
+        attack = Integer.parseInt(stats[4]);
+        defense = Integer.parseInt(stats[5]);
+        speed = Integer.parseInt(stats[6]);
+    }
+
+    public String getName(){return name; }
+    public String getType(){return type; }
+    public int getHp(){return hp;}
+    public int getAttack(){return attack;}
+    public int getDefense(){return defense;}
+    public int getSpeed(){return speed;}
+}
---
+++ src/PokemonMap.java
@@ -0,0 +1,51 @@
+import javax.swing.Timer;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PokemonMap extends JFrame {
+    Timer myTimer;
+    GamePanel game;
+
+    public PokemonMap() {
+        super ("Pokemon Massey"); // window title
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,650); // size
+        game = new GamePanel(this); // defining game
+        myTimer = new Timer(10, new TickListener()); // defining timer
+        setResizable(false);
+        add(game);
+        setVisible(true);
+        start(); // starting timer
+    }
+
+    class TickListener implements ActionListener { // implementing action listener and tick listener
+        public void actionPerformed(ActionEvent evt) {
+            if (game != null) { // if the game is still running
+                game.repaint(); // re-paints the screen
+            }
+        }
+    }
+    public void start() {myTimer.start();} // starts timer
+}
+
+class GamePanel extends JPanel {
+
+    private boolean ready = true;
+    private Image PalletTown;
+
+    public GamePanel(PokemonMap m){
+        PalletTown = new ImageIcon ("windsor.PNG").getImage(); // getting the Pallet Town image
+    }
+
+    public void addNotify() {
+        super.addNotify();
+        requestFocus();
+        ready = true;
+    }
+
+    public void paintComponent (Graphics g){
+        g.drawImage(PalletTown,0,0,null); // drawing the map of windsor
+    }
+}
